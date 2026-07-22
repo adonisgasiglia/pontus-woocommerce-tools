390dac2f56c2f2f3d0c8f819a0ca20b033926db9
Adônis Gasiglia <adonisgasiglia@gmail.com>
2026-07-22 17:51:55 -0300

    Update pontus-woocommerce-tools.php

--- pontus-woocommerce-tools.php
+++ pontus-woocommerce-tools.php
@@ -1,1 +1,23 @@
+<?php
+/**
+ * Plugin Name: Pontus WooCommerce Tools
+ * Plugin URI: https://pontusescritorios.com.br
+ * Description: Ferramentas personalizadas da Pontus para WooCommerce.
+ * Version: 1.0.0
+ * Author: Pontus Escritórios Inteligentes
+ * License: GPL2
+ * Text Domain: pontus-woocommerce-tools
+ */
 
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+define('PWT_VERSION', '1.0.0');
+define('PWT_PLUGIN_FILE', __FILE__);
+define('PWT_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('PWT_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+require_once PWT_PLUGIN_PATH . 'includes/class-plugin.php';
+
+Pontus\WooCommerceTools\Plugin::instance();
